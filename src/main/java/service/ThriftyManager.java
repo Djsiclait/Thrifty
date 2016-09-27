99d1ea2802feed61b9cec3e953b51371ae1adc9f
Djsiclait <djidjellypas92@gmail.com>
2016-09-27 12:38:22 -0400

    Always run the application before git recognizes the changes

--- src/main/java/service/ThriftyManager.java
+++ src/main/java/service/ThriftyManager.java
@@ -5,10 +5,36 @@
  */
 package service;
 
+import Class.Item;
+import Class.User;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Djidjelly Siclait
  */
 public class ThriftyManager {
+    // Attributes 
+    private List<User> users;
+    private List<Item> store;
     
+    // Defining Singleton
+    private ThriftyManager(){
+        setupManagerConfigurations();
+    }
+    
+    // Setup
+    private void setupManagerConfigurations(){
+        // initial setup
+        if (users == null)
+            users = new ArrayList<>();
+        
+        if (store == null)
+            store = new ArrayList<>();
+            
+        // Creating default admin
+        if(users.isEmpty())
+            users.add(new User("admin", "Administrator", "admin", "admin", true));
+    }
 }
