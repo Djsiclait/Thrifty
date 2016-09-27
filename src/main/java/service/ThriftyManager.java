c17d8cb66e5ce65043a4dbca522fdd6196435912
Djsiclait <djidjellypas92@gmail.com>
2016-09-27 12:53:57 -0400

    Add user related functions

--- src/main/java/service/ThriftyManager.java
+++ src/main/java/service/ThriftyManager.java
@@ -35,6 +35,41 @@ public class ThriftyManager {
             
         // Creating default admin
         if(users.isEmpty())
-            users.add(new User("admin", "Administrator", "admin", "admin", true));
+            users.add(new User("ADMIN", "Administrator", "admin", "admin", true));
+    }
+    
+    // Auxiliary Functions
+    public User findUserAccountByUsername(String username){
+        for(User u: users)
+            if(u.getUsername().equals(username.toUpperCase()))
+                return u;
+        
+        return null; // Could not find the user Account
+    }
+    
+    private boolean isUsernameTaken(String username){
+        User user = findUserAccountByUsername(username.toUpperCase());
+        
+        return (user != null);
+    }
+    
+    // User Related Functions
+    public boolean createNewNonAdminUserAccount(String username, String firstName, String lastName, String password){
+        if (!isUsernameTaken(username.toUpperCase())){
+            users.add(new User(username.toUpperCase(), firstName.toUpperCase(), lastName.toUpperCase(), password, false));
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public void deleteNonAdminUserAccount(String username){
+        users.remove(findUserAccountByUsername(username.toUpperCase()));
+    }
+    
+    public boolean validateCredentialsForUserAccount(String username, String password){
+        User user = findUserAccountByUsername(username.toUpperCase());
+        
+        return (user == null) ? false : user.getPassword().equals(password);
     }
 }
